b50bb9ae496c55eb95f13180c1aa3b2003cc0eaa
sarahsamorodnitsky <samor007@umn.edu>
2021-11-06 16:31:40 -0500

    Adding a response and creating the data using the underlying structure

--- bpmf_test.R
+++ bpmf_test.R
@@ -3,13 +3,38 @@
 # -----------------------------------------------------------------------------
 
 # Setting up the data
+n <- 20
+p.vec <- c(100, 150)
+r <- 5
+r.vec <- c(2, 5)
 data <- matrix(list(), ncol = 1, nrow = 2)
-dat1 <- matrix(rnorm(1000), nrow = 100, ncol = 10)
-dat2 <- matrix(rnorm(1500), nrow = 150, ncol = 10)
-data[1,1][[1]] <- dat1
-data[2,1][[1]] <- dat2
 
-Y <- NULL
+V <- matrix(list(), nrow = 1, ncol = 1)
+V[[1,1]] <- matrix(rnorm(n*r, mean = 0, sd = sqrt(10)), nrow = n, ncol = r)
+
+U <- matrix(list(), nrow = q, ncol = 1)
+Vs <- matrix(list(), nrow = 1, ncol = q)
+W <- matrix(list(), nrow = q, ncol = q)
+
+for (s in 1:q) {
+  U[[s,1]] <- matrix(rnorm(p.vec[s]*r, mean = 0, sd = sqrt(10)), nrow = p.vec[s], ncol = r)
+  
+  Vs[[1,s]] <- matrix(rnorm(n*r.vec[s], mean = 0, sd = sqrt(5)), nrow = n, ncol = r.vec[s])
+  
+  W[[s,s]] <- matrix(rnorm(p.vec[s]*r.vec[s], mean = 0, sd = sqrt(5)), nrow = p.vec[s], ncol = r.vec[s])
+  
+  for (ss in 1:q) {
+    if (ss != s) {
+      W[[s,ss]] <- matrix(0, nrow = p.vec[[s]], ncol = r.vec[ss])
+    }
+  }
+}
+
+for (s in 1:q) {
+  data[s,1][[1]] <- U[[s,1]] %*% t(V[[1,1]]) + W[[s,s]] %*% t(Vs[[1,s]]) + matrix(rnorm(p.vec[s]*n), nrow = p.vec[s], ncol = n)
+}
+
+Y <- matrix(rnorm(n), nrow = n)
 
 # Setting up the model parameters
 model_params <- list(error_vars = c(1,1),
