8e66643b2abbed79b0d96242b878b5ea64848cce
sarahsamorodnitsky <samor007@umn.edu>
2021-12-09 17:09:36 -0600

    Starting to set up the simulations. Realize I need to use the other server because it has updated R.

---
+++ bpmf_simulation.R
@@ -0,0 +1,13 @@
+# -----------------------------------------------------------------------------
+# Comparing the performance of BPMF to other low-rank factorization 
+# models. Performance is assessed based on (1) recovery of underlying
+# structure and (2) predictive performance. 
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+# Set up
+# -----------------------------------------------------------------------------
+
+# Loading in the packages
+library(r.jive)
+library(MOFA2)
