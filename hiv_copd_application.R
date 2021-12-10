32dce0cbd229387b4db92d15c36428367d062d11
sarahsamorodnitsky <samor007@umn.edu>
2021-12-10 14:09:55 -0600

    Creating a new data application script for the GitHub. Going to apply the model with and without sparsity.

---
+++ hiv_copd_application.R
@@ -0,0 +1,5 @@
+# -----------------------------------------------------------------------------
+# Applying the BPMF model to the HIV-COPD BALF Biocrates and Somascan data. 
+# Comparing the model application when there is no sparsity vs. when
+# there is sparsity. 
+# -----------------------------------------------------------------------------
