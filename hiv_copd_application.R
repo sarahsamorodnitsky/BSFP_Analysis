5422fa5a5912de1d55f82844e1f3d14fdd123026
sarahsamorodnitsky <samor007@umn.edu>
2021-12-10 14:24:36 -0600

    Adding all the model parameters and setting up the model

--- hiv_copd_application.R
+++ hiv_copd_application.R
@@ -3,3 +3,106 @@
 # Comparing the model application when there is no sparsity vs. when
 # there is sparsity. 
 # -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+# Set-up
+# -----------------------------------------------------------------------------
+
+# Packages
+library(doParallel)
+library(foreach)
+
+# Working directories
+data_wd <- "~/BayesianPMFWithGit/data/"
+model_wd <- "~/BayesianPMFWithGit/"
+
+# Loading in the model functions
+source(paste0(model_wd, "bpmf.R"))
+
+
+# -----------------------------------------------------------------------------
+# Preparing the data
+# -----------------------------------------------------------------------------
+
+# Loading data in 
+load(paste0(data_wd, "BiocratesLavageProcessed.rda"))
+load(paste0(data_wd, "HIV_COPD_SomaScan_Normalized_Clean.rda"))
+
+# BALF Biocrates
+lavage_processed_no_info <- lavage_processed[, -c(1:2)] # removing meta
+bioc_meta_data <- lavage_processed[, 1:2] # saving meta elsewhere
+lavage_processed_no_info_log_scale <- scale(log(lavage_processed_no_info + 1), # log/scale
+                                            center = TRUE, scale = TRUE)
+rownames(lavage_processed_no_info_log_scale) <- bioc_meta_data$Metabolite
+
+# BALF Somascan
+somascan_normalized_clean_no_info <- somascan_normalized_clean[, -c(1:29)] # removing meta
+soma_meta_data <- somascan_normalized_clean[, 1:29] # saving meta elsewhere
+soma_pids <- somascan_normalized_clean$PID
+somascan_normalized_clean_no_info_transpose <- t(somascan_normalized_clean_no_info)
+somascan_normalized_clean_no_info_transpose_scale <- t(apply(somascan_normalized_clean_no_info_transpose, 1, function(row) {
+  scale(log(row + 1), center = TRUE, scale = TRUE)
+})) # scaling the rows
+colnames(somascan_normalized_clean_no_info_transpose_scale) <- soma_pids
+
+# Reorder the Somascan cols to match the Biocrates cols
+somascan_normalized_clean_no_info_transpose_reorder <- 
+  somascan_normalized_clean_no_info_transpose_scale[, match(colnames(lavage_processed_no_info_log_scale),
+                                                            colnames(somascan_normalized_clean_no_info_transpose_scale))]
+
+# Check
+all(colnames(somascan_normalized_clean_no_info_transpose_reorder) == colnames(lavage_processed_no_info_log_scale))
+dim(somascan_normalized_clean_no_info_transpose_reorder)
+dim(lavage_processed_no_info)
+
+# Preparing the response variable
+fev1pp <- subject_processed$FEV1_percent_predicted
+
+# -----------------------------------------------------------------------------
+# Model parameters
+# -----------------------------------------------------------------------------
+
+# Data dimensions
+p1 <- nrow(lavage_processed_no_info_log_scale)
+p2 <- nrow(somascan_normalized_clean_no_info_transpose_reorder)
+n <- ncol(lavage_processed_no_info_log_scale)
+
+# Error variances
+sigma21 <- 1 # error variance for Biocrates
+sigma22 <- 1 # error variance for Somascan
+
+# Variance of joint structure
+lambda_joint <- sqrt(nrow(lavage_processed_no_info_log_scale) + nrow(somascan_normalized_clean_no_info_transpose_reorder)) + 
+  sqrt(ncol(lavage_processed_no_info_log_scale))
+sigma2_joint <- 1/(lambda_joint)
+
+# Variance of individual structure for Biocrates
+lambda_indiv1 <- sqrt(nrow(lavage_processed_no_info_log_scale)) + sqrt(ncol(lavage_processed_no_info_log_scale))
+sigma2_indiv1 <- 1/(lambda_indiv1) 
+
+# Variance of individual structure for Somascan
+lambda_indiv2 <- sqrt(nrow(somascan_normalized_clean_no_info_transpose_reorder)) + sqrt(ncol(lavage_processed_no_info_log_scale))
+sigma2_indiv2 <- 1/(lambda_indiv2)
+
+# For the regression coefficients, beta
+lambda2_intercept <- 1e6
+lambda2_joint <- 1
+lambda2_indiv1 <- 1
+lambda2_indiv2 <- 1
+beta_vars <- c(lambda2_intercept, lambda2_joint, lambda2_indiv1, lambda2_indiv2)
+
+# For the response vector
+shape <- 0.01
+rate <- 0.01
+
+# Putting the model parameters together
+model_params <- list(error_vars = c(sigma21, sigma22),
+                     joint_var = sigma2_joint,
+                     indiv_vars = c(sigma2_indiv1, sigma2_indiv2),
+                     beta_vars = beta_vars,
+                     response_vars = list(shape = shape, rate = rate))
+
+# Gibbs sampler parameters
+nsample <- 5000
+burnin <- nsample/2
+thinned_iters <- seq(burnin, nsample, by = 10)
