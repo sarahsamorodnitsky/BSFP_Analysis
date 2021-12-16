09d084a4555bd6f01436b78feadf6c50e7b358a6
sarahsamorodnitsky <samor007@umn.edu>
2021-12-16 13:59:24 -0600

    Ugh.

--- bpmf.R
+++ bpmf.R
@@ -1882,7 +1882,7 @@ average_results <- function(sim_results, denominator, p.vec, n, q, nsim, results
                                                           avg_ci_width = mean(avg_ci_width_observed_source)),
                                                missing = list(avg_coverage = mean(avg_coverage_missing_source[!is.nan(avg_coverage_missing_source)]),
                                                               avg_mse = mean(avg_mse_missing_source),
-                                                              avg_ci_width = mean(avg_ci_width_missing_source)[!is.nan(avg_ci_width_missing_source)]))
+                                                              avg_ci_width = mean(avg_ci_width_missing_source[!is.nan(avg_ci_width_missing_source)])))
                                                
             }
           }
