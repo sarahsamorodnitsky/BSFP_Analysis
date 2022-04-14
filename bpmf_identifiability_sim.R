1a2d5aa32934b27ee36d8cf5c235ddfc0824d662
sarahsamorodnitsky <samor007@umn.edu>
2022-04-13 20:38:57 -0500

    Change the list of s2ns and also include plotting of the results

--- bpmf_identifiability_sim.R
+++ bpmf_identifiability_sim.R
@@ -41,8 +41,7 @@ nsample <- 2000
 nsim <- 100
 
 # Saving the different potential s2n's for X and Y
-# s2nX.list <- s2nY.list <- c(0.99/0.01, 0.5/0.5, 0.01/0.99)
-s2nX.list <- s2nY.list <- c(1000, 75, 50, 25, 10, 5)
+s2nX.list <- s2nY.list <- c(1000, 0.99/0.01, 75, 50, 25, 10, 5, 0.5/0.5, 0.01/0.99)
 
 # -----------------------------------------------------------------------------
 # Run the simulation with no s2n adjustment in Y
@@ -66,7 +65,7 @@ for (s2nX in s2nX.list) {
   # Save the results
   settings <- list(s2nX = s2nX, s2nY = NULL, init_at_truth = TRUE, num_in_spike = c(2,2,2))
   save(res, settings,
-       file = paste0("~/BayesianPMF/03Simulations/Identifiability/id_sim_s2nX_", s2nX, "_s2nY_NULL_spike222_initattruth_TRUE.rda"))
+       file = paste0("~/BayesianPMF/03Simulations/Identifiability/id_sim_s2nX_", s2nX, "_s2nY_NULL_spike222_initattruth_TRUE_set2.rda"))
   
   # Update counter
   ind <-  ind + 1
@@ -90,7 +89,7 @@ for (s2nX in s2nX.list) {
   # Save the results
   settings <- list(s2nX = s2nX, s2nY = NULL, init_at_truth = TRUE, num_in_spike = c(2,2,2))
   save(res, settings,
-       file = paste0("~/BayesianPMF/03Simulations/Identifiability/id_sim_s2nX_", s2nX, "_s2nY_NULL_spike222_initattruth_FALSE.rda"))
+       file = paste0("~/BayesianPMF/03Simulations/Identifiability/id_sim_s2nX_", s2nX, "_s2nY_NULL_spike222_initattruth_FALSE_set2.rda"))
 
   # Update counter
   ind <-  ind + 1
@@ -106,27 +105,34 @@ for (s2nX in s2nX.list) {
 
 # Initialize a table to contain results
 identifiability_results <- data.frame(s2nX = rep(s2nX.list, 2),
-                                      init_at_truth = rep(c(TRUE, FALSE), each = 3),
-                                      CorrectedSSD = numeric(6),
-                                      NonCorrectedSSD = numeric(6),
-                                      Diff = numeric(6))
+                                      init_at_truth = rep(c(TRUE, FALSE), each = length(s2nX.list)),
+                                      CorrectedSSD = numeric(2*length(s2nX.list)),
+                                      NonCorrectedSSD = numeric(2*length(s2nX.list)),
+                                      Diff = numeric(2*length(s2nX.list)))
 
 # Iterate through the conditions
 for (init_at_truth in c(TRUE, FALSE)) { # Did we initialize at at the true value?
   for (s2nX in s2nX.list) { # What was the s2n for X?
-    # Load in the results
+    # Load in the results from set 1
     load(paste0("~/BayesianPMF/03Simulations/Identifiability/id_sim_s2nX_", s2nX, "_s2nY_NULL_spike222_initattruth_", init_at_truth,".rda"), verbose = TRUE)
     
     # Compute the SSDs
-    corrected_ssd <- mean(res[,1])
-    noncorrected_ssd <- mean(res[,2])
+    corrected_ssd_set1 <- mean(res[,1])
+    noncorrected_ssd_set1 <- mean(res[,2])
+    
+    # Load in the results from set 2
+    load(paste0("~/BayesianPMF/03Simulations/Identifiability/id_sim_s2nX_", s2nX, "_s2nY_NULL_spike222_initattruth_", init_at_truth,"_set2.rda"), verbose = TRUE)
+    
+    # Compute the SSDs
+    corrected_ssd_set2 <- mean(res[,1])
+    noncorrected_ssd_set2 <- mean(res[,2])
     
     # Save the SSDs in the table
     identifiability_results[identifiability_results$s2nX == s2nX & 
-                              identifiability_results$init_at_truth == init_at_truth,]$CorrectedSSD <- corrected_ssd
+                              identifiability_results$init_at_truth == init_at_truth,]$CorrectedSSD <- mean(c(corrected_ssd_set1, corrected_ssd_set2))
     
     identifiability_results[identifiability_results$s2nX == s2nX & 
-                              identifiability_results$init_at_truth == init_at_truth,]$NonCorrectedSSD <- noncorrected_ssd
+                              identifiability_results$init_at_truth == init_at_truth,]$NonCorrectedSSD <- mean(c(noncorrected_ssd_set1, noncorrected_ssd_set2))
   }
 }
 
@@ -136,3 +142,45 @@ identifiability_results$Diff <- identifiability_results$CorrectedSSD - identifia
 # Display results
 library(xtable)
 print(xtable(identifiability_results, digits = 4))
+
+# -----------------------------------------------------------------------------
+# Plot the results
+# -----------------------------------------------------------------------------
+
+library(dplyr)
+
+# Recreate this list for plotting purposes
+s2nX.list.plot <- s2nX.list 
+s2nX.list.plot[9] <- 0.01
+
+# Save the results based on how the model is initialized
+ssd_init_at_truth <- identifiability_results %>% filter(init_at_truth == TRUE) 
+ssd_not_init_at_truth <- identifiability_results %>% filter(init_at_truth == FALSE)
+
+# Change 0.01010101 to 0.01
+ssd_init_at_truth$s2nX[9] <- ssd_not_init_at_truth$s2nX[9] <- 0.01
+
+# Change s2n to factor
+ssd_init_at_truth$s2nX <- factor(ssd_init_at_truth$s2nX)
+ssd_not_init_at_truth$s2nX <- factor(ssd_not_init_at_truth$s2nX)
+
+# Create the plot
+plot(factor(s2nX.list.plot), identifiability_results$CorrectedSSD[1:9], type = "n", 
+     xlab = "Signal-to-Noise in Data", ylab = "Sum of Squared Deviations", 
+     main = "Signal-to-Noise in Data vs. SSD")
+
+# Overlay lines on the plot
+
+# Initialized at the truth
+lines(ssd_init_at_truth$s2nX, ssd_init_at_truth$CorrectedSSD, lwd = 2)
+lines(ssd_init_at_truth$s2nX, ssd_init_at_truth$NonCorrectedSSD, lty = 2, lwd = 2)
+
+# Not initialized at the truth
+lines(ssd_not_init_at_truth$s2nX, ssd_not_init_at_truth$CorrectedSSD, col = 3, lwd = 2)
+lines(ssd_not_init_at_truth$s2nX, ssd_not_init_at_truth$NonCorrectedSSD, lty = 2, col = 3, lwd = 2)
+
+# Add legend
+legend("topright",
+       legend = c("Init at Truth, Corrected", "Init at Truth, Not Corrected", 
+                  "Init Randomly, Corrected", "Init Randomly, Not Corrected"),
+       col = c(1, 1, 3, 3), lty = c(1, 2, 1, 2), cex = 0.5, lwd = c(2, 2, 2, 2))
