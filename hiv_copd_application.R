aff291b0baa29faf1493b5d728ed6a15b2182584
sarahsamorodnitsky <samor007@umn.edu>
2022-06-30 15:44:23 -0500

    Add MOFA with cross validation on HIV-COPD data

--- hiv_copd_application.R
+++ hiv_copd_application.R
@@ -713,6 +713,10 @@ run_model_with_cv(mod = "JIVE", hiv_copd_data = hiv_copd_data, outcome = fev1pp,
 # Running MOFA with cross validation
 # -------------------------------------
 
+run_model_with_cv(mod = "MOFA", hiv_copd_data = hiv_copd_data, outcome = fev1pp,
+                  outcome_name = "FEV1pp", ind_of_pairs = ind_of_pairs, 
+                  model_params = model_params, nsample = nsample)
+
 # -----------------------------------------------------------------------------
 # Cross-Validated Model Fit Results
 # -----------------------------------------------------------------------------
