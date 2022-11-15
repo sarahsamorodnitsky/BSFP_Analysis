3245f91d9fbb22892623791801c31e1bb1c87e1b
sarahsamorodnitsky <samor007@umn.edu>
2022-11-15 17:08:36 -0600

    Add !is.nan() statements to summaries for a binary outcome in the average_results function

--- bsfp.R
+++ bsfp.R
@@ -5471,11 +5471,11 @@ average_results <- function(sim_results, denominator, p.vec, n, q, nsim, results
             avg_ci_width_observed_source <- results_compiled_observed[[3]]/(denominator[[param]][[1,1]])
             
             # Save the results
-            results_for_param[[1,1]] <- list(observed = list(avg_coverage = mean(avg_coverage_observed_source),
-                                                             avg_mse = mean(avg_mse_observed_source),
-                                                             avg_ci_width = mean(avg_ci_width_observed_source)),
+            results_for_param[[1,1]] <- list(observed = list(avg_coverage = mean(avg_coverage_observed_source[!is.nan(avg_coverage_observed_source)]),
+                                                             avg_mse = mean(avg_mse_observed_source[!is.nan(avg_mse_observed_source)]),
+                                                             avg_ci_width = mean(avg_ci_width_observed_source[!is.nan(avg_ci_width_observed_source)])),
                                              missing = list(avg_coverage = mean(avg_coverage_missing_source[!is.nan(avg_coverage_missing_source)]),
-                                                            avg_mse = mean(avg_mse_missing_source),
+                                                            avg_mse = mean(avg_mse_missing_source[!is.nan(avg_mse_missing_source)]),
                                                             avg_ci_width = mean(avg_ci_width_missing_source[!is.nan(avg_ci_width_missing_source)])))
             
           }
